8b28fc1893f49ee643c0979a59698833936c62ff
Sebastin M. Perez <perezsebastianm@gmail.com>
2015-09-29 18:46:10 -0300

    Add files for bundles configuration

---
+++ config/bundles.js
@@ -0,0 +1,10 @@
+var js, css,
+    defaults = require('./defaults.resources'),
+    merge = require('arr-merge');
+
+js = {};
+
+css = {};
+
+exports.js = js;
+exports.css = css;
---
+++ config/defaults.resources.js
@@ -0,0 +1,21 @@
+var mobile, desktop;
+
+mobile = {
+    'js': [],
+    'css': [],
+    'tmpl': [],
+    'fonts': []
+};
+
+desktop = {
+    'js': [],
+    'css': [],
+    'tmpl': [],
+    'fonts': []
+};
+
+/*
+ * Expose
+ */
+exports.mobile = mobile;
+exports.desktop = desktop;
