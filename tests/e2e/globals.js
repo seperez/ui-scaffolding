6b25b4624acf71e261a1cfa6ad8a0b4d8ca719d6
Sebastin M. Perez <perezsebastianm@gmail.com>
2015-09-30 19:11:59 -0300

    Add tests folder

---
+++ tests/e2e/globals.js
@@ -0,0 +1,27 @@
+module.exports = {
+    'retryAssertionTimeout': 10000,
+    'screenshots': './screenshots/',
+    'host': {
+        'mla': {
+            'article': 'http://articulo-desa.mercadolibre.com.ar:8080/',
+            'service': 'http://servicio-desa.mercadolibre.com.ar:8080/',
+            'moto': 'http://moto-desa.mercadolibre.com.ar:8080/',
+            'realEstate': 'http://departamento-desa.mercadolibre.com.ar:8080/'
+        }
+    },
+    'urls': {
+        'mla': {
+            'desktop': {},
+            'mobile': {
+                'core': 'MLA-574286241-_JM',
+                'apparel': 'MLA-579270009-_JM',
+                'services': 'MLA-531085520-_JM',
+                'motor': 'MLA-570475343-_JM',
+                'realEstate': 'MLA-571873830-_JM',
+                'subasta': 'MLA-577735187-_JM',
+                'corePaused': 'MLA-570995227-_JM',
+                'coreClosed': 'MLA-552741430-_JM'
+            }
+        }
+    }
+};
---
+++ tests/unit/hello.spec.js
@@ -0,0 +1,6 @@
+describe('greet module', function () {
+
+    it('should say Hello', function () {
+        expect('Hello').to.equal('Hello');
+    });
+});
